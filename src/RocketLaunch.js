64d8b75354dec5c5ddc6430dbf564fa276375276
PhPersson <51906227+PhPersson@users.noreply.github.com>
2024-07-25 01:30:48 +0200

    Adds rocket image

--- src/RocketLaunch.js
+++ src/RocketLaunch.js
@@ -15,6 +15,7 @@ const RocketLaunch = () => {
     })
     .then(data => {
       const nextLaunch = data.results[0]
+      console.log(nextLaunch);
       setLaunch(nextLaunch)
     })
     .catch(error => {
@@ -31,16 +32,27 @@ const RocketLaunch = () => {
           <div className="rocket-animation">
             <div className="rocket"></div>
           </div>
+
           <h1>{launch.name}</h1>
           <p>Status: {launch.status.name}</p>
           <p>Rocket: {launch.rocket.configuration.name}</p>
-          <p>Mission: {launch.mission.name}  {launch.mission.description}</p>
+          <p>Mission: {launch.mission.name}</p>
+          <p>{launch.mission.description}</p>
           <p>Location: {launch.pad.location.name}, {launch.pad.location.country_code}</p>
           <p>Launch Time: {new Date(launch.net).toLocaleString()}</p>
+
+          <div className="more-info">         
+            {launch.image && (
+            <img src={launch.image} alt={launch.name} className="launch-image" />
+            )}
+          </div>
         </>
+
       )}
     </div>
+    
   );
+  
 }
 
 export default RocketLaunch;
